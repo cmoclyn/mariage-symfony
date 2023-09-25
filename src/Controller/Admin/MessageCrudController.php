3b857a9819e8b5c8651cae8969220daa4f5108e4
c.moclyn <c.moclyn@proxiad.com>
2023-09-25 17:18:49 +0200

    feat: unread message on admin

--- src/Controller/Admin/MessageCrudController.php
+++ src/Controller/Admin/MessageCrudController.php
@@ -3,13 +3,25 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Message;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\Response;
 
 class MessageCrudController extends AbstractCrudController
 {
+    const LIMIT_PREVIEW = 30;
+
     public static function getEntityFqcn(): string
     {
         return Message::class;
@@ -18,23 +30,38 @@ class MessageCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            // set this option if you prefer the page content to span the entire
-            // browser width, instead of the default design which sets a max width
-            ->renderContentMaximized()
+            // ...
+            ->showEntityActionsInlined()
+            ;
+    }
 
-            // set this option if you prefer the sidebar (which contains the main menu)
-            // to be displayed as a narrow column instead of the default expanded design
-            ->renderSidebarMinimized()
+    public function configureAssets(Assets $assets): Assets
+    {
+        return $assets
+            ->addWebpackEncoreEntry('admin')
         ;
     }
 
     public function configureFields(string $pageName): iterable
     {
         return [
-            Field::new('createdAt'),
+            BooleanField::new('isRead')->renderAsSwitch(false),
+            Field::new('createdAt')->setDisabled(),
             Field::new('object'),
-            Field::new('isRead'),
-            Field::new('text')->hideOnIndex(),
+            TextareaField::new('text')->hideOnIndex()
+//                ->formatValue(function($value){
+//                    return substr($value, 0, self::LIMIT_PREVIEW) . '...';
+//                }),
         ];
     }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ;
+    }
+
 }
